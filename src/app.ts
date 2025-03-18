25d4a60e372e8004b81b0f58999e604db59119ec
Plycedes <abhaypratapyadav340@gmail.com>
2025-03-18 21:04:21 +0530

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import express, { Application, Request, Response } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+
+const app: Application = express();
+
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN || "*",
+        credentials: true,
+    })
+);
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+app.get("/", (req: Request, res: Response) => {
+    res.status(200).send({ status: "OK" });
+});
+
+app.get("/api/v1/", (req: Request, res: Response) => {
+    res.status(200).send({ status: "OK" });
+});
---
+++ src/db/index.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const connectDB = async (): Promise<void> => {
+    try {
+        const dbUrl: string = process.env.MONGODB_URL || "";
+        const dbName: string = process.env.DB_NAME || "default";
+
+        const connectionInstance = await mongoose.connect(`${dbUrl}/${dbName}`);
+        console.log(`MongoDB connected! DB_HOST: ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.error("MongoDB connection failed! : ", error);
+        process.exit(1);
+    }
+};
